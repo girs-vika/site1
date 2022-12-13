c3abfdc9a1ddbd4157feda9bf389205d32508441
girs-vika <girss@inbox.ru>
2022-12-13 22:41:16 +0300

    create models

--- my_first/site1/models.py
+++ my_first/site1/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Model1(models.Model):
+    pole1 = models.IntegerField()
+    pole2 = models.CharField(max_length=45)
+    pole3 = models.TextField()
+
+
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+
+
+    def __str__(self):
+        return str(self.pole2) + " " + str(self.pole1)
+
+
+class Model2(models.Model):
+    pole1 = models.IntegerField()
+    pole2 = models.CharField(max_length=45)
+    pole4 = models.ManyToManyField(Model1)
+
+
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+
+    def __str__(self):
+        return str(self.pole2) + " " + str(self.pole1)
